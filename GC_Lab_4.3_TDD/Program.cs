b0e36e6c78ddfd94b47e750b9fbf38f875101f29
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-02-18 17:06:14 -0500

    Completed first pass Interface

--- GC_Lab_4.3_TDD/Program.cs
+++ GC_Lab_4.3_TDD/Program.cs
@@ -6,7 +6,43 @@ namespace GC_Lab_4._3_TDD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let’s locate some primes!");
+            Console.WriteLine();
+            Console.WriteLine("This application will find you any prime, in order, from first prime number on.");
+            
+            do
+            {
+                Console.WriteLine();
+                Console.Write("Which prime number are you looking for?: ");
+                int rank = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                int prime = PrimeNumbers.Rank(rank);
+
+                Console.WriteLine($"The number {rank} prime is {prime}.");
+                Console.WriteLine();
+
+            } while (Continue());
+        }
+
+        private static bool Continue()
+        {
+            while (true)
+            {
+                Console.Write("Do you want to find another prime number? (y / n)");
+                ConsoleKey key = Console.ReadKey(true).Key;
+                Console.WriteLine();
+
+                if (key == ConsoleKey.Y)
+                {
+                    return true;
+                }
+                else if (key == ConsoleKey.N)
+                {
+                    return false;
+                }
+            }
+
         }
     }
 }
