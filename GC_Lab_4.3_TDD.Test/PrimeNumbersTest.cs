38a918fc3ea3048eb5dffd53ecf13e23d955f091
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-02-18 15:47:27 -0500

    Added Failing Tests

--- GC_Lab_4.3_TDD.Test/PrimeNumbersTest.cs
+++ GC_Lab_4.3_TDD.Test/PrimeNumbersTest.cs
@@ -17,5 +17,20 @@ namespace GC_Lab_4._3_TDD.Test
 
         }
 
+        [Theory] // Bears, Beets, Battlestar Galactica
+        [InlineData(1, 2)]
+        [InlineData(2, 3)]
+        [InlineData(3, 5)]
+        [InlineData(4, 7)]
+        [InlineData(5, 11)]
+        public void TestPrimeNumbers(int rank, int expected)
+        {
+
+            int actual = PrimeNumbers.Rank(rank);
+
+            Assert.Equal(expected, actual);
+
+        }
+
     }
 }
